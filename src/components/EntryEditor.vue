33861ebb75484508602ce432dd2f5bb2f7ed0214
Edwin Boon <edwin.boon@eo.nl>
2022-11-04 08:57:33 +0100

    Add emit for creating a journal entry

--- src/components/EntryEditor.vue
+++ src/components/EntryEditor.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
 import { ref, computed } from "vue";
 import EmojiField from "@/components/EmojiField.vue";
-import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg?component";
 import type Emoji from "@/types/Emoji";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg?component";
 
+// data
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
 const maxChars = 280;
 
+// computed
 const charCount = computed(() => text.value.length);
 
+// methods
 const handleTextInput = (e: Event) => {
   const textarea = e.target as HTMLTextAreaElement;
 
@@ -19,10 +24,13 @@ const handleTextInput = (e: Event) => {
     text.value = textarea.value = textarea.value.substring(0, maxChars);
   }
 };
+
+// emits
+defineEmits(["@create"]);
 </script>
 
 <template>
-  <form class="entry-form" @submit.prevent>
+  <form class="entry-form" @submit.prevent="$emit('@create', { text, emoji })">
     <textarea
       :value="text"
       @keyup="handleTextInput"
